845fd0387deed3f516407d5792a4a9bcb6887158
Leo <siruisong97@gmail.com>
2020-01-01 14:39:33 -0500

    can not reconstruct

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 ### Setting up the experiment 
-from data.make_data import generate_trajectory
+# from data.make_data import generate_trajectory
 from models.transformer import *
 from keras import optimizers, losses
 import keras.backend as K
 import numpy as np
-from models.SeqModels import padd_data, state_to_array
-
+from utils import padd_data
+import os
+import tensorflow as tf
 
 '''Generate data'''
-X_observations, true_states = generate_trajectory(num_states=3, 
-                                                  Num_observations=16, 
-                                                  Num_samples=10, 
-                                                  Max_seq=30, 
-                                                  Min_seq=3, 
-                                                  alpha=100,
-                                                  reverse_mode=False)
+# X_observations, true_states = generate_trajectory(num_states=3, 
+#                                                   Num_observations=16, 
+#                                                   Num_samples=5000, 
+#                                                   Max_seq=50, 
+#                                                   Min_seq=30, 
+#                                                   alpha=100,
+#                                                   reverse_mode=False)
+
+# np.save('./data/syn_data.npy',{'X':X_observations, 'states':true_states})
+
+data = np.load('./data/syn_data.npy', allow_pickle = True).item()
+X_observations = data['X']
+true_states = data['states']
+
+'''GPU'''
+os.environ['CUDA_VISIBLE_DEVICES'] = '1' 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 
 
 '''Pad data'''
-max_len = 30
+max_len = 50
 
 X_padded = padd_data(X_observations, max_len)
+X_padded[:,:,0] = np.arange(50)
+mask = X_padded[:,:,1:].mean(axis=-1) != 0
 states_padded = []
 
 for item in true_states:
@@ -32,14 +48,26 @@ states_padded = np.array(states_padded)
 
 
 '''Get model'''
-trans = Transformer(len_limit=30, layers=1, n_head=1)
+trans = Transformer(len_limit=50, time_limit=50, layers=1, n_head=1, d_data=16, d_model=16)
 lr_scheduler = LRSchedulerPerStep(trans.d_model, 4000) 
 # model_saver = ModelCheckpoint(mfile, monitor='ppl', save_best_only=True, save_weights_only=True)
 
-
 trans.compile()
-trans.initial_model(X_padded)
 
-trans.model.fit(X_padded, batch_size=1)
+'''Init'''
+trans.init_model_stage_one(X_padded, batch_size = 32)
 
-# trans.model.evaluate(X_padded[:1])
+pred = trans.recon_model.predict(X_padded[:1])
+
+trans.init_model_stage_two(X_padded)
+
+'''Train'''
+trans.model.fit(X_padded, batch_size=128, epochs = 50)
+
+trans.model.compile(optimizer=Adam(1e-3), metrics = ['acc'])
+results = trans.model.predict(X_padded)
+results = np.argmax(results,-1) # All zero???
+
+
+model = Model(trans.input_xt, trans.enc_mask)
+model.predict(X_padded[:10])[0]
--- utils.py
+++ utils.py
@@ -2,15 +2,34 @@ import keras.backend as K
 from keras.layers import Layer
 # from tensorflow.python.ops.parallel_for.gradients import jacobian
 import tensorflow as tf
+import numpy as np
 
-class pdf_transform_layer(Layer):
-    def call(self, inputs):
-        ht, inc_seq = inputs
-        ht_grad = jacobian(ht, inc_seq)
-        ht_grad = ht_grad[0]
-        self.shape = ht_grad.shape
-        return ht_grad
+
+def padd_data(X, padd_length):
     
-    def compute_output_shape(self, input_shape):
-        # print(input_shape[1])
-        return input_shape[1,:-1]
+    X_padded      = []
+    
+    for k in range(len(X)):
+        
+        if X[k].shape[0] < padd_length:
+            
+            if len(X[k].shape) > 1:
+                X_padded.append(np.array(np.vstack((np.array(X[k]), 
+                                                    np.zeros((padd_length-X[k].shape[0],X[k].shape[1]))
+                                                    ))))
+            else:
+                X_padded.append(np.array(np.vstack((np.array(X[k]).reshape((-1,1)),
+                                                    np.zeros((padd_length-X[k].shape[0],1))
+                                                    ))))
+                
+        else:
+            
+            if len(X[k].shape) > 1:
+                X_padded.append(np.array(X[k]))
+            else:
+                X_padded.append(np.array(X[k]).reshape((-1,1)))
+  
+
+    X_padded      = np.array(X_padded)
+
+    return X_padded
